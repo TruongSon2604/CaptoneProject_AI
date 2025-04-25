3bf21503a951fdfbec904162c7cb41a897fab163
TruongSon2604 <truongngocson26042003@gmail.com>
2025-04-25 15:33:07 +0700

    update v1

--- main.py
+++ main.py
@@ -20,8 +20,6 @@ from fastapi import UploadFile, File
 
 app = FastAPI()
 
-
-
 origins = [
     "http://localhost:5173",
     "http://localhost", 
@@ -47,69 +45,6 @@ def get_weather_data(month, year):
     avg_temperature = data['forecast']['forecastday'][0]['day']['avgtemp_c']
     return avg_temperature
 
-# @app.get("/predict-restock", response_model=list[PredictionResponse])
-# def predict_restock():
-#     query = """
-#     SELECT p.id as product_id, p.name as product_name, p.stock_quantity, c.name as category_name, p.image,
-#            od.quantity, o.created_at, p.price
-#     FROM order_details od
-#     JOIN orders o ON od.orders_id = o.id
-#     JOIN products p ON od.products_id = p.id
-#     JOIN categories c ON c.id = p.categories_id
-#     """
-#     df = pd.read_sql(text(query), engine)
-
-#     df['created_at'] = pd.to_datetime(df['created_at'])
-#     df['month'] = df['created_at'].dt.to_period('M').astype(str)
-
-#     df['is_holiday'] = df['month'].apply(lambda x: 1 if x in ['2025-01', '2025-12'] else 0)
-#     df['is_promotion'] = df['month'].apply(lambda x: 1 if x in ['2025-03', '2025-06','2025-04','2025-05'] else 0) 
-
-#     grouped = df.groupby(['product_id', 'product_name', 'category_name', 'image', 'month']).agg({
-#         'quantity': 'sum', 'price': 'mean', 'is_holiday': 'mean', 'is_promotion': 'mean'
-#     }).reset_index()
-
-#     predictions = []
-
-#     for product_id in grouped['product_id'].unique():
-#         product_data = grouped[grouped['product_id'] == product_id]
-#         # if len(product_data) < 2:
-#         #     continue
-
-#         product_data = product_data.copy()
-#         product_data['month_index'] = range(len(product_data))
-
-#         X = product_data[['month_index', 'price', 'is_holiday', 'is_promotion']]
-#         y = product_data['quantity']
-        
-#         model = LinearRegression()
-#         model.fit(X, y)
-
-#         next_month_index = [[len(product_data), product_data['price'].iloc[-1], 
-#                               product_data['is_holiday'].iloc[-1], product_data['is_promotion'].iloc[-1]]]
-#         predicted_quantity = model.predict(next_month_index)[0]
-
-#         current_stock = int(df[df['product_id'] == product_id]['stock_quantity'].iloc[0])
-#         print(f"Predicted Quantity: {predicted_quantity}, Current Stock: {current_stock}")
-#         predicted_needed = max(int(predicted_quantity), 0)
-
-#         month = product_data['month'].iloc[-1].split('-')[1]
-#         year = product_data['month'].iloc[-1].split('-')[0]
-#         avg_temperature = get_weather_data(int(month), int(year))
-
-#         temperature_adjustment_factor = 1 + (avg_temperature - 25) * 0.05
-#         predicted_needed = int(predicted_needed * temperature_adjustment_factor)
-
-#         predictions.append({
-#             "id": int(product_id),
-#             "name": product_data['product_name'].iloc[0],
-#             "currentStock": current_stock,
-#             "recommendedOrder": predicted_needed,
-#             "image": product_data['image'].iloc[0],
-#             "category": product_data['category_name'].iloc[0]
-#         })
-
-#     return predictions
 @app.get("/predict-restock", response_model=list[PredictionResponse])
 def predict_restock():
     query = """
